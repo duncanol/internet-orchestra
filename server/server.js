5914bb5f1423d5482485335d1dc760f2b5629947
Duncan Watson <duncan.watson@technophobia.com>
2014-01-31 01:10:53 +0000

    Added cheerio package to server-side via meteorite

--- server/server.js
+++ server/server.js
@@ -1,20 +1,57 @@
+lookupSourceCollection = new Meteor.Collection('lookupSources');
+
+var getRandomEntry = function(collection, query) {
+  // TODO - horribly inefficient!
+  var entries = collection.find(query).fetch();
+  var count = entries.length;
+  var i = Math.round(Math.random() * (count - 1));
+  return entries[i];
+};
+
 Meteor.methods({
   getSnippet : function() {
 
-    var count = snippetsCollection.find({}).count();
-    var i = Math.round(Math.random() * (count - 1));
-    var snippet = snippetsCollection.find({}).fetch()[i];
+    var snippet = getRandomEntry(snippetsCollection, {});
     console.log("Returning snippet " + snippet);
     return snippet;
   }
 });
 
+Scraper = function() {
+};
+
+Scraper.prototype.scrapeUrl = function(url, patterns) {
+
+  var text = '<h1>This is a header</h1>\
+    <p>This is a paragraph.</p>';
+
+  snippetsCollection.insert({
+    text : 'This is a paragraph',
+    source : url
+  });
+};
+
 Meteor.startup(function() {
+
+  // var scraper = new Scraper();
+  //
   // var i = 0;
   // Meteor.setInterval(function() {
+  //
+  // var source = getRandomEntry(lookupSources)
+  // scraper.scrapeUrl(url, );
+  //
   // snippetsCollection.insert({
   // text : 'New text inserted ' + (i += 10),
   // source : 'local'
   // });
   // }, 2000);
+
+  // must be
+  // exposed.
+
+  var result = Meteor.http.get("http://www.theonion.com");
+  var html = cheerio.load(result.content);
+  console.log(html.html());
+
 });
