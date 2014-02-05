2d85e6e4e878bbe45942273220d7804807d764ce
Duncan Watson <duncan.watson@technophobia.com>
2014-02-05 08:30:03 +0000

    More converting to coffeescript, and fontawesome package added

--- shared.js
+++
@@ -1,7 +0,0 @@
-snippetsCollection = new Meteor.Collection('snippets');
-
-lookupSourceCollection = new Meteor.Collection('lookupSources');
-
-Meteor.startup(function() {
-  console.log('Ready!');
-});
